77908d775c38f28bc436ec294fc792cf1ddf7a25
Katy Campen <katycampen@gmail.com>
2014-03-25 16:43:15 -0400

    updated form styles

---
+++ app/scripts/log-in.js
@@ -0,0 +1,32 @@
+// can user new User or Parse.User
+$('.js-log-in').click(function(){
+
+// Create new user with Parse parse.
+	var user = new Parse.User();   
+
+	var userName = $('.js-username').val();
+	var emailData = $('.js-email').val();
+	var password = $('.js-password').val();
+	console.log(userName);
+	console.log(emailData); 
+
+	user.set('username', userName);
+	user.set('email', emailData);
+	user.set('password', password);
+
+	user.signUp(null, {
+	success: function(user) {
+		$('.username').val();
+		$('.js-email').val();
+		$('.js-password').val();
+		$('.modal').modal('hide');
+		alert('boom')       
+      },
+      error: function(user, error) {
+      	console.log(error)
+      	alert('error')
+        
+      }
+      });
+
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,37 +4,7 @@ Parse.initialize("bCi5dBHvGtOYnVzhkhS8f0GFjeEKElpFEt2IIVeO", "0JO22w3zsha7daj0NC
 
 console.log('welcome to the space jam');
 
-$('.js-log-in').click(function(){
 
-// can user new User or Parse.User
-	var user = new Parse.User();   
-
-	var userName = $('.js-username').val();
-	var emailData = $('.js-email').val();
-	var password = $('.js-password').val();
-	console.log(userName);
-	console.log(emailData); 
-
-	user.set('username', userName);
-	user.set('email', emailData);
-	user.set('password', password);
-
-	user.signUp(null, {
-	success: function(user) {
-		$('.username').val();
-		$('.js-email').val();
-		$('.js-password').val();
-		$('.modal').modal('hide');
-		alert('boom')       
-      },
-      error: function(user, error) {
-      	console.log(error)
-      	alert('error')
-        
-      }
-      });
-
-});
 
 });
 
